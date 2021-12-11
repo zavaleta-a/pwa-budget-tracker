31853d56451803451ebfecd311a09deb463de49c
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-11 17:37:12 -0600

    adding code that will update database

---
+++ public/indexedDb.js
@@ -0,0 +1,7 @@
+let db;
+const request = indexedDB.open("budget-tracker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
