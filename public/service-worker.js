29819fb7e08d11b5920b7abe41a89c9b5614a312
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-11 15:37:18 -0600

    adding code for offline first approach

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,14 +38,36 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
           return fetch(evt.request)
-            .then(response => {
+            .then((response) => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  //Serving static assets using offline-first approach.  request is not for the API.
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
