51c11bfcbc37812f5de590b4c060a8f44df8facc
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-11 17:42:06 -0600

    updating code to heroku

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -5,3 +5,10 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("new_transaction", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
